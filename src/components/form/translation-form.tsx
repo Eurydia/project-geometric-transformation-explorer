fcb7c8cec3aa3d93d800f6161bf0f20eb0998736
Eurydia <crossdeltavi@gmail.com>
2025-09-12 12:09:59 +0200

    Update translation-form.tsx

--- src/components/form/translation-form.tsx
+++ src/components/form/translation-form.tsx
@@ -1,24 +1,36 @@
 import { Fragment, type FC } from "react";
-import { useForm } from "@tanstack/react-form";
-import {
-  alpha,
-  Button,
-  Grid,
-  OutlinedInput,
-  Stack,
-  TextField,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import { createFormHook } from "@tanstack/react-form";
+import { alpha, Stack, Toolbar, Typography } from "@mui/material";
 import { MathJax } from "better-react-mathjax";
 import z from "zod/v4";
+import { fieldContext, formContext } from "@/contexts/app-form-context";
+import { ArrayItemAddButton } from "../form-input/array-item-add-button";
+import { ArrayItemRemoveButton } from "../form-input/arrat-item-remove-button";
+import { FormResetButton } from "../form-input/form-reset-button";
+import { FormSubmitButton } from "../form-input/form-submit-button";
+import { NumberTextField } from "../form-input/NumberTextField";
+
+const { useAppForm } = createFormHook({
+  fieldComponents: {
+    ArrayItemAddButton,
+    ArrayItemRemoveButton,
+    NumberTextField,
+  },
+  formComponents: {
+    FormResetButton,
+    FormSubmitButton,
+  },
+  fieldContext: fieldContext,
+  formContext: formContext,
+});
 
 const NumericString = z
   .string()
   .trim()
   .nonempty()
   .normalize()
-  .refine((arg) => !isNaN(Number(arg)));
+  .refine((arg) => !isNaN(Number(arg)))
+  .pipe(z.transform((arg) => Number(arg)));
 
 export const TranslationFormDataSchema = z.object({
   points: z.object({ x: NumericString, y: NumericString }).array().max(4),
@@ -29,7 +41,7 @@ type Props = {
   onSubmit: (v: z.output<typeof TranslationFormDataSchema>) => unknown;
 };
 export const TranslationForm: FC<Props> = ({ onSubmit }) => {
-  const { Field, handleSubmit, Subscribe, reset, pushFieldValue } = useForm({
+  const { FormResetButton, FormSubmitButton, AppField, AppForm } = useAppForm({
     defaultValues: {
       points: [{ x: "1", y: "1" }],
       translation: { x: "2", y: "1" },
@@ -38,193 +50,80 @@ export const TranslationForm: FC<Props> = ({ onSubmit }) => {
       onChange: TranslationFormDataSchema,
     },
     onSubmit: ({ value }) => {
-      onSubmit(value);
+      const r = TranslationFormDataSchema.safeParse(value);
+      if (r.success) {
+        onSubmit(r.data);
+      }
     },
   });
 
   return (
     <Stack>
-      <Toolbar sx={{ justifyContent: "space-between" }}>
-        <Subscribe selector={({ isDefaultValue }) => ({ isDefaultValue })}>
-          {({ isDefaultValue }) => (
-            <Button
-              color="error"
-              variant="outlined"
-              onClick={() => reset()}
-              disabled={isDefaultValue}
-            >
-              {`คืนค่าเริ่มต้น`}
-            </Button>
-          )}
-        </Subscribe>
+      <Toolbar>
+        <AppForm>
+          <FormResetButton />
+        </AppForm>
       </Toolbar>
-      <Grid padding={1} container spacing={0.5}>
-        <Grid size={{ sm: 4, xs: 12 }}>
-          <Typography>
-            <MathJax>{`ขนาดการเลื่อนขนาน $(x,y)$`}</MathJax>
-          </Typography>
-        </Grid>
-        <Grid size={{ sm: 8, xs: 12 }}>
-          <Stack useFlexGap spacing={0.5} direction={"row"} flexWrap={"nowrap"}>
-            <Field name="translation.x">
-              {(field) => (
-                <TextField
-                  error={field.state.meta.errors.length > 0}
-                  value={field.state.value}
-                  onChange={(e) => field.handleChange(e.target.value)}
-                  onBlur={field.handleBlur}
-                  slotProps={{
-                    htmlInput: {
-                      sx: { fontFamily: "monospace" },
-                      type: "number",
-                    },
-                  }}
-                  sx={{ flexBasis: 0, flexGrow: 1 }}
-                />
-              )}
-            </Field>
-            <Field name="translation.y">
-              {(field) => (
-                <TextField
-                  error={field.state.meta.errors.length > 0}
-                  value={field.state.value}
-                  onChange={(e) => field.handleChange(e.target.value)}
-                  onBlur={field.handleBlur}
-                  slotProps={{
-                    htmlInput: {
-                      sx: { fontFamily: "monospace" },
-                      type: "number",
-                    },
-                  }}
-                  sx={{ flexBasis: 0, flexGrow: 1 }}
-                />
-              )}
-            </Field>
-          </Stack>
-        </Grid>
-      </Grid>
+      <Stack spacing={0.5} padding={1}>
+        <Typography>
+          <MathJax>{`ขนาดการเลื่อนขนาน $(x,y)$`}</MathJax>
+        </Typography>
+        <Stack useFlexGap spacing={0.5} direction={"row"} flexWrap={"nowrap"}>
+          <AppField name="translation.x">
+            {(field) => <field.NumberTextField />}
+          </AppField>
+          <AppField name="translation.y">
+            {(field) => <field.NumberTextField />}
+          </AppField>
+        </Stack>
+      </Stack>
       <Stack>
-        <Field name="points" mode="array">
+        <AppField name="points" mode="array">
           {(field) => (
             <Fragment>
               {field.state.value.map((_, index) => (
-                <Grid
+                <Stack
                   key={`translate-point-${index}`}
                   spacing={0.5}
                   padding={1}
-                  container
                   sx={{
                     backgroundColor: ({ palette: { primary } }) =>
-                      index % 2 === 0 ? undefined : alpha(primary.light, 0.08),
+                      index % 2 === 1 ? undefined : alpha(primary.light, 0.08),
                   }}
                 >
-                  <Grid size={{ sm: 4, xs: 12 }}>
-                    <Stack
-                      width={"100%"}
-                      spacing={0.5}
-                      direction={{ xs: "row", sm: "column" }}
-                      useFlexGap
-                      justifyContent={{ xs: "space-between" }}
-                    >
-                      <Typography>
-                        <MathJax>{`พิกัดที่ ${index + 1}`}</MathJax>
-                      </Typography>
-                      <Typography
-                        sx={{
-                          cursor:
-                            field.state.value.length > 1
-                              ? "pointer"
-                              : undefined,
-                        }}
-                        color={
-                          field.state.value.length > 1
-                            ? "error"
-                            : "textDisabled"
-                        }
-                        component={"div"}
-                        tabIndex={0}
-                        onClick={() => {
-                          if (field.state.value.length > 1) {
-                            field.removeValue(index);
-                          }
-                        }}
-                        width={"fit-content"}
-                      >
-                        (ลบ)
-                      </Typography>
-                    </Stack>
-                  </Grid>
-                  <Grid size={{ sm: "grow", xs: 12 }}>
-                    <Stack spacing={0.5} direction="row">
-                      <Field name={`points[${index}].x`}>
-                        {(subField) => (
-                          <OutlinedInput
-                            error={subField.state.meta.errors.length > 0}
-                            value={subField.state.value}
-                            onChange={(e) =>
-                              subField.handleChange(e.target.value)
-                            }
-                            onBlur={subField.handleBlur}
-                            slotProps={{
-                              input: {
-                                sx: { fontFamily: "monospace" },
-                                type: "number",
-                              },
-                            }}
-                            sx={{ flexBasis: 0, flexGrow: 1 }}
-                          />
-                        )}
-                      </Field>
-                      <Field name={`points[${index}].y`}>
-                        {(subField) => (
-                          <OutlinedInput
-                            error={subField.state.meta.errors.length > 0}
-                            value={subField.state.value}
-                            onChange={(e) =>
-                              subField.handleChange(e.target.value)
-                            }
-                            onBlur={subField.handleBlur}
-                            slotProps={{
-                              input: {
-                                sx: { fontFamily: "monospace" },
-                                type: "number",
-                              },
-                            }}
-                            sx={{ flexBasis: 0, flexGrow: 1 }}
-                          />
-                        )}
-                      </Field>
-                    </Stack>
-                  </Grid>
-                </Grid>
+                  <Stack
+                    width={"100%"}
+                    spacing={0.5}
+                    direction={"row"}
+                    useFlexGap
+                    justifyContent={{ xs: "space-between" }}
+                  >
+                    <Typography>
+                      <MathJax>{`พิกัดที่ ${index + 1}`}</MathJax>
+                    </Typography>
+                    <field.ArrayItemRemoveButton index={index} />
+                  </Stack>
+                  <Stack spacing={0.5} direction="row">
+                    <AppField name={`points[${index}].x`}>
+                      {(subField) => <subField.NumberTextField />}
+                    </AppField>
+                    <AppField name={`points[${index}].x`}>
+                      {(subField) => <subField.NumberTextField />}
+                    </AppField>
+                  </Stack>
+                </Stack>
               ))}
             </Fragment>
           )}
-        </Field>
+        </AppField>
       </Stack>
       <Toolbar sx={{ justifyContent: "space-between" }}>
-        <Subscribe selector={({ canSubmit }) => ({ canSubmit })}>
-          {({ canSubmit }) => (
-            <Button
-              variant="contained"
-              onClick={handleSubmit}
-              disabled={!canSubmit}
-            >
-              {`คำนวณ`}
-            </Button>
-          )}
-        </Subscribe>
-        <Subscribe selector={({ values }) => ({ values })}>
-          {({ values }) => (
-            <Button
-              disabled={values.points.length >= 4}
-              variant="outlined"
-              onClick={() => pushFieldValue("points", { x: "", y: "" })}
-            >
-              {`เพิ่มพิกัด`}
-            </Button>
-          )}
-        </Subscribe>
+        <AppForm>
+          <FormSubmitButton />
+        </AppForm>
+        <AppField name="points">
+          {(field) => <field.ArrayItemAddButton />}
+        </AppField>
       </Toolbar>
     </Stack>
   );
